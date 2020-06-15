3ef9646488b3456e512c37390ebe7420668e48e1
Anastasia <slyadneva.anastasia@gmail.com>
2020-06-15 11:40:52 -0500

    Testing build

--- src/ui/ResumeButton.js
+++ src/ui/ResumeButton.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import resume from '../assets/Anastasia_Slyadneva_Resume.pdf'
+// import resume from '../assets/Anastasia_Slyadneva_Resume.pdf'
 
 const ResumeButton = () => {
   return (
-    <a href={resume} download="Anastasia_Slyadneva_Resume" className="ResumeButton">Resume</a>
+    <a href="https://google.com" className="ResumeButton">Resume</a>
   )
 }
 
